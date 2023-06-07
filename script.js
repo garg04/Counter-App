fde3765ca718d10f3d16b08b18e28ba0cbee98e5
garg04 <gauravgarg375@gmail.com>
2023-06-07 15:43:56 +0530

    changes in decrement handler

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const incrementbtn = document.querySelector('.increment');
 const decrement = document.querySelector('.decrement');
+const inputelement = document.querySelector('.inc-dec input');
 
 let counter = document.querySelector('.counter-value h3');
 let counter_value = parseInt(counter.innerText);
+let incdecby = 0;
 
 // increment handler
 incrementbtn.addEventListener('click', function () {
@@ -12,12 +14,18 @@ incrementbtn.addEventListener('click', function () {
 
 // decrement handler
 decrement.addEventListener('click', function () {
-    counter_value = counter_value - 1;
-    if (counter_value < 0) {
+    if (counter_value < 1) {
         return;
     }
+
+    counter_value = counter_value - 1;
     counter.innerText = counter_value;
 });
 
-
-
+// input change handler
+inputelement.addEventListener("change", function(e) {
+    if(incdecby != "") {
+        console.timeLog("jgdas");
+        incdecby = e.target.value;
+    }
+})
